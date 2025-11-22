9558dd47b53705932e6826507c3f9ad53d2d78b2
jesusdscientist <sanan.cafarov.94@gmail.com>
2025-11-22 22:18:59 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/ternaryOperator.java
@@ -0,0 +1,10 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+
+        String ishiqforRengi = "qırmızı";
+
+        String kecheBileremmi = (ishiqforRengi == "yaşıl") ? "keç" : "gözlə";
+
+        System.out.println("Keçə bilərəmmi? : " + kecheBileremmi);
+    }
+}
