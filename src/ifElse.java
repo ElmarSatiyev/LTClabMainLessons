cd288f846e6de471171739d68632b80ed3471590
jesusdscientist <sanan.cafarov.94@gmail.com>
2025-11-22 20:28:28 +0400

    if else nümunəsi Dərs3

--- src/ifElse.java
+++ src/ifElse.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ifElse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bir eded dsxil et: ");
+        System.out.println("Bir eded daxil et: ");
         int number = sc.nextInt();
 
         if (number > 0) {
