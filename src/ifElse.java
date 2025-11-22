31427c81f893b5e91ddf1d1c48606351e143e845
jesusdscientist <sanan.cafarov.94@gmail.com>
2025-11-22 20:12:07 +0400

    if else nümunəsi Dərs3

---
+++ src/ifElse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ifElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bir eded dsxil et: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("Bu eded musbetdir..");
+        } else if (number < 0) {
+            System.out.println("Bu eded menfidir..");
+        } else {
+            System.out.println("Bu eded 0-dir.");
+        }
+    }
+
+}
