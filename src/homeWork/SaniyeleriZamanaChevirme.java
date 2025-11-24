563d7435dbb4104e9ae03e81ada1367bc3d9624f
jesusdscientist <sanan.cafarov.94@gmail.com>
2025-11-24 12:05:26 +0400

    Saniyələri Zamana Çevirmə

---
+++ src/homeWork/SaniyeleriZamanaChevirme.java
@@ -0,0 +1,35 @@
+package homeWork;
+
+import java.util.Scanner;
+
+public class SaniyeleriZamanaChevirme {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hesablacaq müddəti daxil edin (saniyə): ");
+        long toplam_saniye = input.nextLong();
+
+        int saat = (int) (toplam_saniye / 3600);
+        int deqiqe = (int) ((toplam_saniye % 3600) / 60);
+        int saniye = (int) ((toplam_saniye % 3600) % 60);
+
+        String result = "";
+
+        if (saat > 0) {
+            result += saat + " saat ";
+        }
+        if (deqiqe > 0) {
+            result += deqiqe + " dəqiqə ";
+        }
+        if (saniye > 0) {
+            result += saniye + " saniyə";
+        }
+
+        // Hamısı sıfır olarsa sadəcə "0 saniyə" çap edək
+        if (result.isEmpty()) {
+            result = "0 saniyə";
+        }
+
+        System.out.println(result.trim());
+
+    }
+}
