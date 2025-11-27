4e64c369b0b7f570b6b335b9aaf26ede0528b44f
jesusdscientist <sanan.cafarov.94@gmail.com>
2025-11-27 21:18:06 +0400

    Mentor - Lesson4

---
+++ src/Loops/BookShelver.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class BookShelver {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kitabların sayını daxil edin");
+        int count = sc.nextInt();
+
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Kitab " + i + " nömrəli rəfə qoyuldu və nömrələndi.");
+        }
+        System.out.println("Bütün " + count + " kitab rəfə yerləşdirildi.");
+    }
+}
