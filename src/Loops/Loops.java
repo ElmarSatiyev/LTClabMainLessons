6f6f6e9b1e73d0e3e9d6fd75c3387eb1daa96e7b
jesusdscientist <sanan.cafarov.94@gmail.com>
2025-11-29 13:36:51 +0400

    Mentor - Lesson4

---
+++ src/Loops/Loops.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        // for (başlanğıc; sert; sayaç)
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Salam " + i);
+            }
+
+        }
+    }
+}
+
